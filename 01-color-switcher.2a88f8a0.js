88abc6efe1b1e4391ea5d7bb429636ace9cb61cb
vladishevv <67576630+vladishevv@users.noreply.github.com>
2022-12-29 05:05:19 +0000

    Deploying to gh-pages from @ vladishevv/goit-js-hw-09@05ba2972ebde9e7fc7280cd7ec8c81b453181063 🚀

---
+++ 01-color-switcher.2a88f8a0.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r=null;t.addEventListener("click",(function(){t.setAttribute("disabled","true"),e.removeAttribute("disabled"),r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.setAttribute("disabled","true"),clearInterval(r)})),console.log(o.style.backgroundColor);
+//# sourceMappingURL=01-color-switcher.2a88f8a0.js.map
